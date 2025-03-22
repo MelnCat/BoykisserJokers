121752c545d8f84f1a9c063641e095539fe1bc92
MelnCat <melncatuwu@gmail.com>
2025-03-22 15:57:36 -0700

    update

---
+++ main.lua
@@ -0,0 +1,27 @@
+AltTexture {
+	key = "jokers",
+    set = "Joker",
+	path = "Jokers.png",
+	original_sheet = true,
+	keys = {"j_joker", "j_greedy_joker", "j_lusty_joker", "j_wrathful_joker", "j_gluttenous_joker", "j_jolly", "j_zany", "j_mad", "j_crazy", "j_droll", "j_sly", "j_wily", "j_clever", "j_devious", "j_crafty", "j_half", "j_stencil", "j_mime", "j_marble", "j_misprint", "j_chaos", "j_steel_joker", "j_abstract", "j_hack", "j_even_steven", "j_odd_todd", "j_scholar", "j_ride_the_bus", "j_space", "j_egg", "j_burglar", "j_blackboard", "j_runner", "j_blue_joker", "j_sixth_sense", "j_constellation", "j_hiker", "j_faceless", "j_green_joker", "j_todo_list", "j_card_sharp", "j_madness", "j_square", "j_riff_raff", "j_vampire", "j_hologram", "j_vagabond", "j_baron", "j_midas_mask", "j_luchador", "j_photograph", "j_hallucination", "j_fortune_teller", "j_stone", "j_golden", "j_baseball", "j_trading", "j_flash", "j_selzer", "j_mr_bones", "j_swashbuckler", "j_certificate", "j_smeared", "j_throwback", "j_glass", "j_ring_master", "j_blueprint", "j_wee", "j_merry_andy", "j_matador", "j_hit_the_road", "j_stuntman", "j_invisible", "j_brainstorm", "j_drivers_license", "j_cartomancer", "j_astronomer", "j_burnt"},
+    loc_txt = {
+        name = "BoykisserJokers Jokers"
+    }
+}
+TexturePack {
+    key = "boykisserjokers",
+    textures = {
+        "boykisserjokers_jokers",
+    },
+    loc_txt = {
+        name = "BoykisserJokers",
+        text = {"Turns all jokers into boykissers."}
+    }
+}
+
+SMODS.Atlas {
+    key = "modicon",
+    path = "icon.png",
+    px = 34,
+    py = 34
+}
